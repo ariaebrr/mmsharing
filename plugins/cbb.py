487853c502ddec7f2158d012c326adf387ed3226
ariaebrr <132585315+ariaebrr@users.noreply.github.com>
2024-11-19 21:53:56 +0300

    cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,8 +10,9 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={5498499669}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={5498499669}'>This Person</a>\n○ Channel : @FrostVibes</b>",
             disable_web_page_preview = True,
+
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
