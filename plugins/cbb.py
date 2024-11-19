8de1fe652b9c2ab41c98d94cb48a7352c1b3d7bb
ariaebrr <132585315+ariaebrr@users.noreply.github.com>
2024-11-20 01:55:45 +0300

    cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={5498499669}'>This Person</a>\n○ Channel : @FrostVibes</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={5498499669}'>Frost</a>\n○ Channel : @FrostVibes</b>",
             disable_web_page_preview = True,
 
             reply_markup = InlineKeyboardMarkup(
